7c0e4524ab8bbe74f3b441faaf2cd3adb4b8aa12
Luke Zhang <lukezha@amazon.com>
2025-10-21 10:34:31 -0700

    update otel version in package.json file.

--- zRun.sh
+++ zRun.sh
@@ -4,7 +4,12 @@ rm -rf ./node_modules
 rm -fr ./aws-distro-opentelemetry-node-autoinstrumentation/build
 rm -fr ./aws-distro-opentelemetry-node-autoinstrumentation/node_modules
 
-npm ci
+# npm ci
+
+npm install
+
 npm run lint
 
 npm run compile
+
+npm run test
